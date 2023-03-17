9fdb6c2b195c7a600fba303c1079b0838b4883ac
wandellAZ <wandelldacostamota@gmail.com>
2023-03-17 16:25:11 -0400

    finalizado

--- wa.dart
+++ wa.dart
@@ -1,12 +1,16 @@
 import 'dart:io';
 
 void main() {
-  var c1 = [];
+  //"var"(vai declara uma variavel) com uma lista com 10 vetores.
+  var List = [0,0,0,0,0,0,0,0,0,0,0];
   print('insira os números:');
-  for (int i = 0; i < 10; i++) {
-    int c2 = int.parse(stdin.readLineSync()!);
-    c1.add(c2);
+  //"for"(vai deixar em loop o programa até o fim da execução)
+  //"int i = 0; i < List.length; i++"(Impõe o limite de vezes que o "i" vai se repetir, e o "List.length" mostrar o tamanho da lista ou vetor)
+  for (int i = 0; i < List.length; i++) {
+    int Valor = int.parse(stdin.readLineSync()!);
+    List[i] = Valor;
   }
-  c1.removeAt(5);
-  print("Aqui está:${c1}");
+  List.removeAt(5);
+  print("Aqui está:${List}");
+  print(List.length);
 }
